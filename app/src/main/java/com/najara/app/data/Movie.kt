11f3f87c13a6a18de14c3e32bd1c08c26185dfe4
sandip945235-hub <sandip945235@gmail.com>
2026-09-23 22:31:58 +0530

    Create Movie.kt

---
+++ app/src/main/java/com/najara/app/data/Movie.kt
@@ -0,0 +1,9 @@
+package com.najara.app.data
+
+data class Movie(
+    val title: String,
+    val poster: String,
+    val category: String,
+    val embedLink: String,
+    val downloadLink: String
+)
